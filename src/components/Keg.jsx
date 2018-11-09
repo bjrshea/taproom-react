11ac1a81d31c011d6783b21ebe31965b1f9e2527
Brendan Shea <padma@email.com>
2018-11-09 11:25:18 -0800

    Create loop in KegList component.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Keg() {
+function Keg(props) {
   return(
     <div>
       <h1>Test</h1>
     </div>
   );
 }
+
 
 export default Keg;
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,63 +1,69 @@
 import React from 'react';
 import Keg from './Keg';
 
-const kegList = {
-  masterKegList: [
-    {
-      name: 'Vaporizer',
-      brewer: 'Double Mountain',
-      type: 'IPA',
-      abv: '7.2%',
-      price: '6',
-      remaining: '124'
-    },
-    {
-      name: 'What Rough Beast',
-      brewer: 'Breakside',
-      type: 'IPA',
-      abv: '6.8%',
-      price: '$5',
-      remaining: '124'
-    },
-    {
-      name: 'Turmoil',
-      brewer: 'Barley Brown',
-      type: 'Cascadian Dark Ale',
-      abv: '7.8%',
-      price: '7',
-      remaining: '124'
-    },
-    {
-      name: 'Bodhizafa',
-      brewer: 'Georgetown',
-      type: 'IPA',
-      abv: '6.9%',
-      price: '6',
-      remaining: '124'
-    },
-    {
-      name: 'Straight Outta Portland',
-      brewer: 'Migration',
-      type: 'IPA',
-      abv: '7.3%',
-      price: '6',
-      remaining: '124'
-    },
-    {
-      name: 'Chocolate Shake',
-      brewer: 'Boulder Beer',
-      type: 'Porter',
-      abv: '5.9%',
-      price: '5',
-      remaining: '124'
-    }
-  ]
-};
+const masterKegList = [
+  {
+    name: 'Vaporizer',
+    brewer: 'Double Mountain',
+    type: 'IPA',
+    abv: '7.2%',
+    price: '6',
+    pints: '124'
+  },
+  {
+    name: 'What Rough Beast',
+    brewer: 'Breakside',
+    type: 'IPA',
+    abv: '6.8%',
+    price: '$5',
+    pints: '124'
+  },
+  {
+    name: 'Turmoil',
+    brewer: 'Barley Brown',
+    type: 'Cascadian Dark Ale',
+    abv: '7.8%',
+    price: '7',
+    pints: '124'
+  },
+  {
+    name: 'Bodhizafa',
+    brewer: 'Georgetown',
+    type: 'IPA',
+    abv: '6.9%',
+    price: '6',
+    pints: '124'
+  },
+  {
+    name: 'Straight Outta Portland',
+    brewer: 'Migration',
+    type: 'IPA',
+    abv: '7.3%',
+    price: '6',
+    pints: '124'
+  },
+  {
+    name: 'Chocolate Shake',
+    brewer: 'Boulder Beer',
+    type: 'Porter',
+    abv: '5.9%',
+    price: '5',
+    pints: '124'
+  }
+];
 
 function KegList() {
   return(
     <div>
-      <Keg/>
+      {masterKegList.map((keg, index) =>
+        <Keg
+          name={keg.name}
+          brewer={keg.brewer}
+          type={keg.type}
+          abv={keg.abv}
+          price={keg.price}
+          pints={keg.pints}/>
+      )}
     </div>
   );
 }
