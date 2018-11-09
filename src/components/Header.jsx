6da018f7d76f13f2e4b9324ad774d599e3a0c6ac
Brendan Shea <padma@email.com>
2018-11-09 09:40:29 -0800

    Add keg logo to header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import keg from '../assets/images/keg.png';
 
 function Header() {
   return(
     <div>
-      
+      <img src={keg}/>
+      <h2>Barrel House</h2>
     </div>
   )
 }
