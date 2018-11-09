0037f11b234908fd8af863652d5c006cd21387f5
Brendan Shea <padma@email.com>
2018-11-09 11:40:49 -0800

    Add props to Keg component and effectively loop through props.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -4,10 +4,23 @@ import PropTypes from 'prop-types';
 function Keg(props) {
   return(
     <div>
-      <h1>Test</h1>
+      <h1>{props.name}</h1>
+      <h2>{props.brewer}</h2>
+      <h3>{props.type}</h3>
+      <h3>{props.abv}</h3>
+      <h3>{props.price}</h3>
+      <h3>{props.pints}</h3>
     </div>
   );
 }
 
+Keg.propTypes = {
+  name: PropTypes.string,
+  brewer: PropTypes.string,
+  type: PropTypes.string,
+  abv: PropTypes.string,
+  price: PropTypes.number,
+  pints: PropTypes.number
+};
 
 export default Keg;
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -7,48 +7,48 @@ const masterKegList = [
     brewer: 'Double Mountain',
     type: 'IPA',
     abv: '7.2%',
-    price: '6',
-    pints: '124'
+    price: 6,
+    pints: 124
   },
   {
     name: 'What Rough Beast',
     brewer: 'Breakside',
     type: 'IPA',
     abv: '6.8%',
-    price: '$5',
-    pints: '124'
+    price: 5,
+    pints: 124
   },
   {
     name: 'Turmoil',
     brewer: 'Barley Brown',
     type: 'Cascadian Dark Ale',
     abv: '7.8%',
-    price: '7',
-    pints: '124'
+    price: 7,
+    pints: 124
   },
   {
     name: 'Bodhizafa',
     brewer: 'Georgetown',
     type: 'IPA',
     abv: '6.9%',
-    price: '6',
-    pints: '124'
+    price: 6,
+    pints: 124
   },
   {
     name: 'Straight Outta Portland',
     brewer: 'Migration',
     type: 'IPA',
     abv: '7.3%',
-    price: '6',
-    pints: '124'
+    price: 6,
+    pints: 124
   },
   {
     name: 'Chocolate Shake',
     brewer: 'Boulder Beer',
     type: 'Porter',
     abv: '5.9%',
-    price: '5',
-    pints: '124'
+    price: 5,
+    pints: 124
   }
 ];
 
@@ -62,7 +62,8 @@ function KegList() {
           type={keg.type}
           abv={keg.abv}
           price={keg.price}
-          pints={keg.pints}/>
+          pints={keg.pints}
+          key={index}/>
       )}
     </div>
   );
