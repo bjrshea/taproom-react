20787aa6fde3d9bd53686c7c9adba680eab829c7
Brendan Shea <padma@email.com>
2018-11-09 16:47:54 -0800

    Add buttons to admin portal for admins to use.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -12,9 +12,6 @@ function Admin() {
           margin-bottom: 7%;
         }
         .admin-info {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
           width: 380px;
           height: 450px;
           border: 2px solid #2B303A;
@@ -22,9 +19,18 @@ function Admin() {
           background-color: #7A7265;
           color: #F7F3E3;
         }
+        .button-box {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 50%;
+        }
       `}</style>
       <div className="admin-info">
-        
+        <div className="button-box">
+          <button>Sell a pint</button>
+          <button>Edit pint's price</button>
+        </div>
       </div>
     </div>
   );
