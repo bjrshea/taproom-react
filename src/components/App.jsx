482cbb14b1098d7f8a86557e031a0311409373e6
Brendan Shea <padma@email.com>
2018-11-16 08:23:14 -0800

    Lifted state to the App component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,15 @@ import Admin from './Admin';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
-function App(){
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+     masterKegList: []
+   };
+  }
+  
   return (
     <div>
       <style jsx global>{`
@@ -26,3 +34,60 @@ function App(){
 }
 
 export default App;
+
+// const masterKegList = [
+//   {
+//     name: 'Vaporizer',
+//     brewer: 'Double Mountain',
+//     location: 'Portland, OR',
+//     type: 'IPA',
+//     abv: '7.2%',
+//     price: 6,
+//     pints: 124
+//   },
+//   {
+//     name: 'What Rough Beast',
+//     brewer: 'Breakside',
+//     location: 'Portland, OR',
+//     type: 'IPA',
+//     abv: '6.8%',
+//     price: 5,
+//     pints: 124
+//   },
+//   {
+//     name: 'Turmoil',
+//     brewer: 'Barley Brown',
+//     location: 'Baker City, OR',
+//     type: 'Cascadian Dark Ale',
+//     abv: '7.8%',
+//     price: 7,
+//     pints: 124
+//   },
+//   {
+//     name: 'Bodhizafa',
+//     brewer: 'Georgetown',
+//     location: 'Seattle, WA',
+//     type: 'IPA',
+//     abv: '6.9%',
+//     price: 6,
+//     pints: 124
+//   },
+//   {
+//     name: 'Straight Outta Portland',
+//     brewer: 'Migration',
+//     location: 'Portland, OR',
+//     type: 'IPA',
+//     abv: '7.3%',
+//     price: 6,
+//     pints: 124
+//   },
+//   {
+//     name: 'Chocolate Shake',
+//     brewer: 'Boulder Beer',
+//     location: 'Boulder, CO',
+//     type: 'Porter',
+//     abv: '5.9%',
+//     price: 5,
+//     pints: 124
+//   }
+// ];
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,63 +1,6 @@
 import React from 'react';
 import Keg from './Keg';
 
-const masterKegList = [
-  {
-    name: 'Vaporizer',
-    brewer: 'Double Mountain',
-    location: 'Portland, OR',
-    type: 'IPA',
-    abv: '7.2%',
-    price: 6,
-    pints: 124
-  },
-  {
-    name: 'What Rough Beast',
-    brewer: 'Breakside',
-    location: 'Portland, OR',
-    type: 'IPA',
-    abv: '6.8%',
-    price: 5,
-    pints: 124
-  },
-  {
-    name: 'Turmoil',
-    brewer: 'Barley Brown',
-    location: 'Baker City, OR',
-    type: 'Cascadian Dark Ale',
-    abv: '7.8%',
-    price: 7,
-    pints: 124
-  },
-  {
-    name: 'Bodhizafa',
-    brewer: 'Georgetown',
-    location: 'Seattle, WA',
-    type: 'IPA',
-    abv: '6.9%',
-    price: 6,
-    pints: 124
-  },
-  {
-    name: 'Straight Outta Portland',
-    brewer: 'Migration',
-    location: 'Portland, OR',
-    type: 'IPA',
-    abv: '7.3%',
-    price: 6,
-    pints: 124
-  },
-  {
-    name: 'Chocolate Shake',
-    brewer: 'Boulder Beer',
-    location: 'Boulder, CO',
-    type: 'Porter',
-    abv: '5.9%',
-    price: 5,
-    pints: 124
-  }
-];
-
 function KegList() {
   return(
     <div className="box-container">
